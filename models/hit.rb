f2e7e6914a3bf542869dcbcdf6cbbea5964e146a
Jonathan C <chapmajs@gmail.com>
2015-11-23 10:47:27 -0500

    Avoiding race condition on first hit for a given IP

--- models/hit.rb
+++ models/hit.rb
@@ -1,10 +1,16 @@
 class Hit < ActiveRecord::Base
   belongs_to :counter
 
+  validates :address, :uniqueness => {:scope => :counter_id}
+
   # BEGIN Class Methods
 
   def self.process_hit(request, counter)
-    find_or_create_by(:address => request.ip, :counter => counter, :ipv6 => request.ip.include?(':')).touch
+  	begin
+    	find_or_create_by(:address => request.ip, :counter => counter, :ipv6 => request.ip.include?(':')).touch
+    rescue ActiveRecord::RecordNotUnique
+    	retry
+    end
   end
 
   # END Class Methods
