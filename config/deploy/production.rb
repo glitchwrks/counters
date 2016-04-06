613d90504c0c271b55651d8dc14f00727ab76289
Jonathan C <chapmajs@gmail.com>
2016-04-06 16:59:06 -0400

    Trying string key for env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,10 +4,9 @@ set :default_env, { :path => "$HOME/.gem/ruby/2.2/bin:$PATH" }
 
 server 'services.theglitchworks.net', user: 'services', roles: %w{app db web}, my_property: :my_value
 set :unicorn_rack_env, :production
-set :rack_env, :production
 set :unicorn_config_path, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "/var/run/unicorn/site_services.pid"
 set :linked_files, %w{config/database.yml config/email.yml}
-set :rack_env, :production
+set :rack_env, 'production'
 
 after 'deploy', 'unicorn:reload'
