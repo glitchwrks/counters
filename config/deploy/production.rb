6c7a8d3876904e92ac1974fff4741b8abdf905fc
Jonathan C <chapmajs@gmail.com>
2016-04-06 22:09:16 -0400

    Apparently this is the magic place to set RACK_ENV

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,14 +1,12 @@
 set :stage, :production
+set :rack_env, :production
 
-set :default_env, { :path => "$HOME/.gem/ruby/2.2/bin:$PATH" }
+set :default_env, { :path => "$HOME/.gem/ruby/2.2/bin:$PATH", :rack_env => :production }
 
 server 'services.theglitchworks.net', user: 'services', roles: %w{app db web}, my_property: :my_value
 set :unicorn_rack_env, :production
 set :unicorn_config_path, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "/var/run/unicorn/site_services.pid"
 set :linked_files, %w{config/database.yml config/email.yml}
-set :rack_env, :production
-set :rails_env, :production
-set :environment, :production
 
 after 'deploy', 'unicorn:reload'
