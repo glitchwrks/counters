9c7ef460de91caa836a5463bccedaefe4faf417b
Jonathan Chapman <chapmajs@gmail.com>
2017-01-16 17:38:34 -0500

    Adding DB seeds file

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+# Minimal setup for development
+
+Counter.create(:name => 'test_counter', :ipv4_preload => 123, :ipv6_preload => 456)
+SitewideCounter.create(:name => 'main', :ipv4_preload => 321, :ipv6_preload => 654)
