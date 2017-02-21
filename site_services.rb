eec00e5127032fdd05b0e529cf4b092156780646
Jonathan Chapman <chapmajs@gmail.com>
2017-02-21 10:25:37 -0500

    Testing json_csrf enabled for rack protection

--- site_services.rb
+++ site_services.rb
@@ -8,7 +8,6 @@ require_all 'services'
 class SiteServices < Sinatra::Base
   enable :logging
   set :port, '8080'
-  set :protection, :except => [:json_csrf]
 
   get '/counters/:name' do
     counter = Counter.find_by(:name => params[:name])
