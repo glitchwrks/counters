53f89240498ca24f90a0acceb90bc0735cd22b35
Jonathan Chapman <chapmajs@gmail.com>
2019-04-03 20:49:18 -0400

    Moving to new server, removing old shared config files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,10 +3,10 @@ set :rack_env, :production
 
 set :default_env, { :path => "$HOME/.gem/ruby/2.2/bin:$PATH", :rack_env => :production }
 
-server 'services.theglitchworks.net', user: 'services', roles: %w{app db web}, my_property: :my_value
+server 'services.glitchworks.net', user: 'services', roles: %w{app db web}, my_property: :my_value
 set :unicorn_rack_env, :production
 set :unicorn_config_path, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "/var/run/unicorn/site_services.pid"
-set :linked_files, %w{config/database.yml config/email.yml config/recaptcha.yml}
+set :linked_files, %w{config/database.yml}
 
 after 'deploy', 'unicorn:reload'
