fc68ce3f959790fee460997a15502acb225c307d
Jonathan Chapman <chapmajs@gmail.com>
2023-01-14 18:59:32 -0500

    Expanding Counter factory definitions

--- spec/factories/counter_definitions.rb
+++ spec/factories/counter_definitions.rb
@@ -2,6 +2,24 @@ FactoryBot.define do
 
   factory :counter do
     sequence(:name) { |n| "counter_#{n}"} 
+    ipv4_preload { 0 }
+    ipv6_preload { 0 }
+  end
+
+  factory :counter_with_hits, :parent => :counter do
+    after(:create) do |c|
+      FactoryBot.create(:hit, :counter_id => c.id)
+      FactoryBot.create(:hit, :ipv6, :counter_id => c.id)
+    end
+  end
+
+  factory :counter_with_many_hits, :parent => :counter do
+    after(:create) do |c|
+      25.times do
+        FactoryBot.create(:hit, :counter_id => c.id)
+        FactoryBot.create(:hit, :ipv6, :counter_id => c.id)
+      end
+    end
   end
 
 end
