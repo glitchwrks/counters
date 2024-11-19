9be5c39412197a70131bc98b64fb65dfbfe80fc7
Jonathan Chapman <glitch@glitchwrks.com>
2024-11-19 16:22:25 -0500

    WIP new staging server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,9 @@
 # config valid only for current version of Capistrano
-lock '3.17.1'
+lock '3.19.2'
 
 set :application, 'site_services'
 set :repo_url, 'git@github.com:chapmajs/site_services.git'
-set :deploy_to, '/home/services/site_services'
+set :deploy_to, '/home/counters/site_services'
 set :keep_releases, 2
 set :rvm_type, :user
-set :rvm_ruby_version, '3.1.3@site_services'
+set :rvm_ruby_version, '3.3.6@site_services'
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,11 +1,11 @@
 set :stage, :production
 set :rack_env, :production
 
-set :branch, :openbsd6_upgrade
+set :branch, :master
 
 set :default_env, { :path => "$HOME/.gem/ruby/2.3/bin:$PATH", :rack_env => :production }
 
-server 'services.bv.theglitchworks.net', user: 'services', roles: %w{app db web}, my_property: :my_value
+server 'openbsd-test.map.glitchworks.net', user: 'counters', roles: %w{app db web}, my_property: :my_value
 set :unicorn_rack_env, :production
 set :unicorn_config_path, "#{current_path}/config/unicorn.rb"
 set :unicorn_pid, "/var/run/unicorn/site_services.pid"
